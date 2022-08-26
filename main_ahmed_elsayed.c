f4213030cae7a16d2d45a9c90ea4e6f48a345000
Ahmed Sayed Ali <mrahmedsayedali@gmail.com>
2022-08-26 19:42:29 +0200

    First Main File In My Branch 🦾🦾

---
+++ main_ahmed_elsayed.c
@@ -0,0 +1,14 @@
+/*
+By : Ahmed Elsayed Abdulrahman Ali
+Email : mrahmedsayedali@gmail.com
+github : https://github.com/MrAhmedSayedAli
+*/
+
+#include <stdio.h>
+
+int main() {
+	    printf("Hi Iam Ahmed Elsayed 😎 \n");
+//---------------------------------------------------------------->
+
+    return 0;
+}
